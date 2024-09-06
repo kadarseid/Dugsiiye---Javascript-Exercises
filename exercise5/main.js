26735a42a224099bf9a4621e1385eb4a06129c6b
Kadar <masiciid2@gmail.com>
2024-09-06 10:50:44 +0300

    add an exercise 5 - arrow function

---
+++ exercise5/main.js
@@ -0,0 +1,8 @@
+// arrow function
+const add3 = (a, b) => a + b;
+
+console.log(add3(3, 6)); // 9
+console.log(add3(4, 12)); // 16
+console.log(add3(9, 2)); // 11
+console.log(add3(25, 11)); // 36
+console.log(add3(37, 8)); // 45
