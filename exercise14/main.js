146501f301bd1b4dbf490329119f002941c73a53
Kadar <masiciid2@gmail.com>
2024-09-06 18:14:09 +0300

    add an exercise 14 - do while lopp

---
+++ exercise14/main.js
@@ -0,0 +1,8 @@
+let numbers;
+do {
+  number = prompt("Enter a number greater than 10?");
+} while (number <= 10);
+
+console.log(
+  "You entered number " + number + " that is greater than 10; Thank you!"
+);
