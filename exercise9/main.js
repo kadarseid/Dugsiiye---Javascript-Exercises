ff21357c128339b7c62235ba8d34594a4dbab56d
Kadar <masiciid2@gmail.com>
2024-09-06 11:15:35 +0300

    add an exercise 9 - methods in objects

---
+++ exercise9/main.js
@@ -0,0 +1,11 @@
+let car = {
+  make: "Toyota",
+  model: "Land Cruiser",
+  year: "2024",
+  color: "black",
+  start: function () {
+    console.log("The car has started");
+  },
+};
+
+car.start(); // The car has started
