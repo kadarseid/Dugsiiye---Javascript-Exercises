ef1b89b51c44ebfc4baf708b8248d231d9df7450
Kadar <masiciid2@gmail.com>
2024-09-06 11:11:54 +0300

    add another problem and solution to exercise 8 - objects

--- exercise8/main.js
+++ exercise8/main.js
@@ -7,3 +7,9 @@ let car = {
 console.log(car.make); // Output: Toyota
 console.log(car.model); // Output: Land Cruiser
 console.log(car.year); // Output: 2007
+
+car.color = "black";
+console.log(car); // {make: 'Toyota', model: 'Land Cruiser', year: 2007, color: 'black'}
+
+car.year = 2024;
+console.log(car); // {make: 'Toyota', model: 'Land Cruiser', year: 2024, color: 'black'}
