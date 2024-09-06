a136dcd1ce447107f8afed3656ef987a09a3a075
Kadar <masiciid2@gmail.com>
2024-09-06 10:46:08 +0300

    add exercise 4 - function declaration and expression

---
+++ exercise4/main.js
@@ -0,0 +1,17 @@
+// function declaration
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(4, 15)); // 19
+console.log(add(18, 5)); // 23
+console.log(add(33, 7)); // 40
+console.log(add(8, 9)); // 17
+
+// function expression
+const add2 = function (a, b) {
+  return a + b;
+};
+
+console.log(add2(10, 5)); // 15
+console.log(add2(30, 5)); // 35
