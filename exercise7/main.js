2f0292b0b1c110731ed1b03b962b67b334f83a07
Kadar <masiciid2@gmail.com>
2024-09-06 11:00:34 +0300

    add exercise 7 -  array methods

---
+++ exercise7/main.js
@@ -0,0 +1,9 @@
+let colors = ["red", "green", "blue"];
+
+console.log(colors); // Output: (3) ['red', 'green', 'blue']
+
+colors.push("yellow");
+console.log(colors); // Output: (3) ['red', 'green', 'blue', 'yellow']
+
+colors.shift();
+console.log(colors); // Output: (3) ['green', 'blue', 'yellow']
