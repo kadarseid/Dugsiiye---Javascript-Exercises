c95d5c09e5e9f303ec95b6fb2b3fc2081090dcb5
Kadar <masiciid2@gmail.com>
2024-09-06 11:04:14 +0300

    add an exercise 8 - objects

---
+++ exercise8/main.js
@@ -0,0 +1,9 @@
+let car = {
+  make: "Toyota",
+  model: "Land Cruiser",
+  year: 2007,
+};
+
+console.log(car.make); // Output: Toyota
+console.log(car.model); // Output: Land Cruiser
+console.log(car.year); // Output: 2007
