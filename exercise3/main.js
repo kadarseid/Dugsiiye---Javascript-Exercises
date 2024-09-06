72c961ba96f9c528d2ca9628a8ea3f9616da00fc
Kadar <masiciid2@gmail.com>
2024-09-06 10:38:07 +0300

    add an exercise 3 - logical operators

---
+++ exercise3/main.js
@@ -0,0 +1,7 @@
+let isSunny = true;
+let isWeekend = false;
+
+console.log(isSunny && isWeekend); // Output: False
+console.log(isSunny || isWeekend); // Output: True
+console.log(!isSunny); // Output: False
+console.log(!isWeekend); // Output: True
