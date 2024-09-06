eb9fcd28bef2f1f3f7d59469cbd204c9d22ad631
Kadar <masiciid2@gmail.com>
2024-09-06 10:25:40 +0300

    add an exercise 2 - comparison operators

---
+++ exercise2/main.js
@@ -0,0 +1,11 @@
+let a = 15;
+let b = "15";
+
+console.log(a == b); // Output: True
+console.log(a != b); // Output: false
+console.log(a === b); // Output: false
+console.log(a !== b); // Output: True
+console.log(a > b); // Output: false
+console.log(a < b); // Output: false
+console.log(a >= b); // Output: True
+console.log(a <= b); // Output: True
