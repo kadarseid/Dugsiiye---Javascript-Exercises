a810bb947a0e0418346f4a344f489281282d2db2
Kadar <masiciid2@gmail.com>
2024-09-06 11:20:36 +0300

    add an exercise 10 - arrays in objects

---
+++ exercise10/main.js
@@ -0,0 +1,25 @@
+let library = [
+  {
+    title: "Lost and Founder",
+    author: "Rand Fishkin",
+    year: 2018,
+  },
+  {
+    title: "The 22 immutable laws of branding",
+    author: "Al Ries",
+    year: 2002,
+  },
+  {
+    title: "Hooked",
+    author: "Nir Eyal",
+    year: 2014,
+  },
+  {
+    title: "The 1-page Marketing Plan",
+    author: "Allan Dib",
+    year: 2016,
+  },
+];
+
+console.log(library[0].title); // Output: Lost and Founder
+console.log(library[1].author); // Output: Al Ries
