cc114da829bc59ea9185aea86b43dcfe35f254c2
Kadar <masiciid2@gmail.com>
2024-09-06 10:54:51 +0300

    add an exercise 6 -  arrays

---
+++ exercise6/main.js
@@ -0,0 +1,6 @@
+let colors = ["red", "green", "blue"];
+
+console.log(colors); // Output: (3) ["red", "green", "blue"]
+console.log(colors[0]); // Output: Red
+console.log(colors[1]); // Output: green
+console.log(colors[2]); // Output: blue
