6e21380803ea25b9b1099e5153a87feca82a33af
Kadar <masiciid2@gmail.com>
2024-09-17 04:25:21 +0300

    add an exercise 32 - DOM selecting elements

---
+++ exercise32/main.js
@@ -0,0 +1,8 @@
+const question = document.querySelector(".question");
+
+const title = document.querySelectorAll(".title");
+
+console.log(question);
+console.log(title);
+console.log(title[0]);
+console.log(title[1]);
