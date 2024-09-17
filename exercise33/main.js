e58ce995ff3259995076e99e4a00a203c40d198b
Kadar <masiciid2@gmail.com>
2024-09-17 04:46:09 +0300

    add an exercise 33 -  DOM manipulating Elements

---
+++ exercise33/main.js
@@ -0,0 +1,9 @@
+function changeContent() {
+  const title = document.querySelector(".title");
+  title.textContent = "Welcome to Javascript Exercise - 32";
+}
+
+function changeElement() {
+  const topic = document.querySelector(".topic");
+  topic.innerHTML = `<span>Manipulating Elements</span>`;
+}
